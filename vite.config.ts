3f33c5684062d4c0fcd2233fa9fbfc7cf2c04d64
donizer <bogdan.ua45@gmail.com>
2024-01-07 00:27:51 +0200

    try base/

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/vite_react_phone-catalog/",
+  base: "/",
   plugins: [react()],
 });
